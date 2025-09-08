7dcfd7b3cc8e6ab496236d76a19fb2b95b38abe2
Peter Cross <itextx@gmail.com>
2025-09-09 02:34:54 +0300

    deleting some trash

--- src/components/AppActionBar.vue
+++ src/components/AppActionBar.vue
@@ -1,15 +1,12 @@
 <template>
   <ActionBar>
-    <!-- <ActionBarIcon icon="chat-o" text="Icon1" />
-    <ActionBarIcon icon="cart-o" text="Icon2" />
-    <ActionBarIcon icon="shop-o" text="Icon3" /> -->
     <ActionBarButton color="#be99ff" type="warning" text="Себе" />
     <ActionBarButton color="#7232dd" type="danger" text="Другому" />
   </ActionBar>
 </template>
 
 <script setup>
-import { ActionBar, ActionBarIcon, ActionBarButton } from 'vant';
+import { ActionBar, ActionBarButton } from 'vant';
 </script>
 
 <style scoped>
