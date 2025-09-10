2d1a82452c707c5aa1cbc0b8eb44d65a8d4d49e0
Peter Cross <itextx@gmail.com>
2025-09-11 01:12:53 +0300

    adding tabbar route mode

--- src/components/AppTabBar.vue
+++ src/components/AppTabBar.vue
@@ -1,29 +1,9 @@
 <template>
-  <van-tabbar v-model="active">
-    <van-tabbar-item
-      replace
-      to="/"
-      icon="home-o"
-      >Бронь</van-tabbar-item
-    >
-    <van-tabbar-item
-      replace
-      to="/journal"
-      icon="search"
-      >Журнал</van-tabbar-item
-    >
-    <van-tabbar-item
-      replace
-      to="/floor"
-      icon="friends-o"
-      >Этажи</van-tabbar-item
-    >
-    <van-tabbar-item
-      replace
-      to="/room"
-      icon="setting-o"
-      >КИР</van-tabbar-item
-    >
+  <van-tabbar v-model="active" route safe-area-inset-bottom>
+    <van-tabbar-item replace to="/" icon="home-o">Бронь</van-tabbar-item>
+    <van-tabbar-item replace to="/journal" icon="search">Журнал</van-tabbar-item>
+    <van-tabbar-item replace to="/floor" icon="friends-o">Этажи</van-tabbar-item>
+    <van-tabbar-item replace to="/room" icon="setting-o">КИР</van-tabbar-item>
   </van-tabbar>
 </template>
 
