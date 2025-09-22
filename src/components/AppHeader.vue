0e18043ea29d1c77506cbe22b7b18fae05600d1e
Peter Cross <itextx@gmail.com>
2025-09-23 01:11:37 +0300

    feat: adding drawer

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,15 +9,60 @@
     <van-button
       color="transparent"
       icon="info-o"
+      @click="showDrawer = true"
     />
+    <van-popup
+      v-model:show="showDrawer"
+      position="right"
+      closeable
+      class="drawer"
+    >
+      <van-image
+        width="90%"
+        style="padding-left: 16px"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFQhweSX-arDvu4jqlftQqNfhowjHD6yyTaw&s"
+      />
+
+      <van-cell-group>
+        <van-cell
+          v-for="link in drawerLinks"
+          :key="link.title"
+          :title="link.title"
+          is-link
+          :url="link.url"
+        />
+      </van-cell-group>
+    </van-popup>
   </header>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import router from '../router';
 import { useStateStore } from '../stores/state';
 
 const stateStore = useStateStore();
+
+const showDrawer = ref(false);
+
+const drawerLinks = [
+  {
+    title: 'Сообщение об ошибке',
+    url: 'https://github.com',
+  },
+  {
+    title: 'Оставить отзыв',
+    url: 'https://gitlab.com',
+  },
+  {
+    title: 'Соглашение',
+    url: 'https://bitbucket.com',
+  },
+  {
+    title: 'Инструкция',
+    url: 'https://stackoverflow.com',
+  },
+];
 </script>
 
 <style scoped>
@@ -31,5 +76,14 @@ const stateStore = useStateStore();
 h1 {
   margin: 0;
   color: white;
+}
+
+.drawer {
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  width: 80%;
+  height: 100%;
+  padding-top: 60px;
 }
 </style>
