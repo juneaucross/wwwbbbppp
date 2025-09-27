69096279fac8b6446c3c5a8708c20379934a4620
Peter Cross <itextx@gmail.com>
2025-09-28 01:39:19 +0300

    feat: adding vuecal

--- src/views/RoomView.vue
+++ src/views/RoomView.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1>КИР</h1>
+  <!-- <h1>КИР</h1> -->
+  <vue-cal
+    v-bind="config"
+    editable-events
+    @event-create="createEvent"
+  />
 </template>
+
+<script setup>
+import { VueCal } from 'vue-cal';
+import 'vue-cal/style';
+
+const config = {
+  hideWeekends: true,
+  titleBar: false,
+  views: ['day'],
+  viewsBar: false,
+  snapToInterval: 30,
+  timeFrom: 9 * 60,
+  timeTo: 18 * 60,
+  timeStep: 30,
+  // time: true,
+  // sm: true,
+  // xs: true,
+};
+
+const createEvent = ({ event, resolve }) => {
+  resolve({
+    ...event,
+    title: 'New Event! 🎉',
+  });
+};
+</script>
